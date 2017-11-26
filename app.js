1e5947d66204a5c9f42cde483cf0674383fb1634
chirag jain <chiragiem36@gmail.com>
2017-11-27 00:51:00 +0530

    just save

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+var app = express()
+var mongo = require('mongodb').MongoClient
+var bp = require('body-parser')
+
+var db
+
+mongo.connect('mongodb://localhost:27018/data', (err, dbs) => {
+  if (!err) {
+    db = dbs
+  } else {
+    console.log(err)
+  }
+})
 
 app.use('/',express.static('public'))
+app.get('/enquiry', bp.json(), (req,res) => {
+  console.log(req.body);
+  db.collection('enquiry').insert(req.body)
+  res.status(200)
+  res.end()
+})
 
 app.listen(process.env.PORT || '80', () => {
   console.log('server is listening on port 80')
